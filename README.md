ae9cca2f311f27451f31d9a589de7209f1cdcd51
Valery Vargin <vdvugad@gmail.com>
2019-02-08 09:08:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# babel-plugin-react-intl-extractor
+
+Merge descriptors of messages from 
+[babel-plugin-react-intl](https://github.com/yahoo/babel-plugin-react-intl) into single file.
+
+Also creates lang files, prepared for usage as messages
+in [IntlProvider](https://github.com/yahoo/react-intl/wiki/Components#intlprovider). 
+
+## Dependencies
+
+### Babel Plugin React Intl
+This Babel plugin works with Babel Plugin React Intl v3.x
+  
+### Babel
+This plugin works with Babel 7
+
+## Installation
+
+```sh
+$ npm install --save-dev babel-plugin-react-intl-extractor
+```
+or
+```sh
+$ yarn add --dev babel-plugin-react-intl-extractor
+```
+
+## Usage
+
+**This Babel plugin only visits ES6 modules which `import` React Intl.**
+
+The default message descriptors for the app's default language will 
+be extracted from: `defineMessages()`, `<FormattedMessage>`, and `<FormattedHTMLMessage>`; all of which are named exports of the React Intl package.
+
+If a message descriptor has a `description`, it'll be removed 
+from the source after it's extracted to save bytes since it isn't used at runtime.
+
+### Via `customize-cra` or `react-app-rewired` (Recommended)
+
+**config-overrides.js**
+
+```javascript
+const {addBabelPlugin, override} = require('customize-cra')
+
+module.exports = override(
+  ...
+  addBabelPlugin(['react-intl', {enforceDescriptions: false}]),
+  addBabelPlugin(['react-intl-extractor', {extractedFile: './src/translations/aggregated.json'}]),
+  ...
+)
+```
+
+### Via `.babelrc`
+
+**.babelrc**
+
+```json
+{
+  "plugins": [
+    [
+      "react-intl", {
+        "messagesDir": ""
+      }
+    ],
+    [
+      "react-intl-extractor",
+      {
+        "extractedFile": "./src/translations/aggregated.json",
+        "langFiles": [
+          "./src/translations/en.json"
+        ]
+      }
+    ]
+  ]
+}
+```
+
+### Notice
+You can provide any parameter to react-intl plugin, except `moduleSourceName`,
+because this plugin expects only "react-intl" value for param `moduleSourceName`
+
+#### Options
+
+- **`extractedFile`**: The target location where the plugin will output a descriptors for each component from which React Intl messages were extracted. Default: "./src/translations/aggregated.json"
+
+- **`langFiles`**: The target location where the plugin will output a messages (Object.<id, string>) for each component from which React Intl messages were extracted. Default: ["./src/translations/en.json", "./src/translations/ru.json"]
+
+### If you use [babel-plugin-react-intl-auto](https://github.com/akameco/babel-plugin-react-intl-auto)
+You must use plugins in that order:
+1. babel-plugin-react-intl-auto
+2. babel-plugin-react-intl
+3. babel-plugin-react-intl-extractor
---
+++ src/index.js
@@ -0,0 +1,110 @@
+import fs from 'fs'
+
+export default function (pluginOptions) {
+  const options = Object.assign(
+    {
+      extractedFile: './src/translations/aggregated.json',
+      langFiles: [
+        {path: './src/translations/ru.json', cleanUpNewMessages: false},
+        {path: './src/translations/en.json', cleanUpNewMessages: true},
+      ],
+    },
+    pluginOptions,
+  )
+
+  /**
+   * @returns {Array}
+   */
+  function getTargetFileContent () {
+    const doesFileExists = fs.existsSync(options.extractedFile)
+
+    if (!doesFileExists) {
+      return []
+    }
+
+    const result = JSON.parse(fs.readFileSync(options.extractedFile))
+
+    return Array.isArray(result) ? result : []
+  }
+
+  /**
+   * @returns {Object.<string, string>}
+   */
+  function getTargetLangFileContent (path) {
+    const doesFileExists = fs.existsSync(path)
+
+    if (!doesFileExists) {
+      return {}
+    }
+
+    return Object.assign({}, JSON.parse(fs.readFileSync(path)))
+  }
+
+  /**
+   * @param {{id: string}[]} existing
+   * @param {{id: string}[]} descriptors
+   *
+   * @returns {{id: string}[]}
+   */
+  function mergeDescriptors (existing, descriptors) {
+    return existing
+      .concat(descriptors)
+      .filter((d, i, a) => a.indexOf(d) === i)
+      .sort((a, b) => {
+        return (a.id < b.id) ? -1 : (a.id > b.id ? 1 : 0)
+      })
+  }
+
+  function mergeMessages (cleanUpNewMessages, oldMessages, newMessages) {
+    if (!cleanUpNewMessages) {
+      return Object.assign(oldMessages, newMessages)
+    }
+
+    const result = Object.assign({}, oldMessages)
+
+    Object
+      .keys(newMessages)
+      .forEach(
+        (id) => {
+          if (!result[id]) {
+            result[id] = ''
+          }
+        },
+      )
+
+    return result
+  }
+
+  return {
+    post: function (file) {
+      if (!file.metadata['react-intl']?.messages?.length) {
+        return
+      }
+
+      const descriptors = file.metadata['react-intl'].messages
+      if (!Array.isArray(descriptors)) {
+        return
+      }
+
+      const existingDescriptors = getTargetFileContent()
+      const resultDescriptors = mergeDescriptors(existingDescriptors, descriptors)
+
+      if (!resultDescriptors?.length) {
+        return
+      }
+
+      fs.writeFileSync(options.extractedFile, JSON.stringify(resultDescriptors, null, 2))
+
+      const messages = {}
+      resultDescriptors.forEach(({id, defaultMessage}) => {
+        messages[id] = defaultMessage || ''
+      })
+
+      options.langFiles.forEach(({path, cleanUpNewMessages}) => {
+        const resultMessages = mergeMessages(cleanUpNewMessages, getTargetLangFileContent(path), messages)
+
+        fs.writeFileSync(path, JSON.stringify(resultMessages, null, 2))
+      })
+    },
+  }
+}
